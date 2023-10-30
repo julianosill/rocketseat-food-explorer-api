8c6bb3062372a45c5ebb72abdeeaa3df6cc3f159
Juliano Sill <sill.juliano@gmail.com>
2023-10-30 20:59:15 -0300

    create exception errors handler (AppError)

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const cors = require('cors')
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const AppError = require('./utils/AppError')
 
 const api = express()
 api.use(express.json())
@@ -14,6 +15,22 @@ api.use(
     credentials: true,
   })
 )
+
+api.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+
+  console.error(error)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 const PORT = process.env.PORT
 api.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
