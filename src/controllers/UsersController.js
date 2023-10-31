00cc415cb23dfc525ea4b490799bf46755ff34fc
Juliano Sill <sill.juliano@gmail.com>
2023-10-31 20:14:00 -0300

    style(UsersController): clean empty spaces

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,33 +6,27 @@ const UserDeleteService = require('../services/UserDeleteService')
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body
-
     const usersRepository = new UsersRepository()
     const userCreateService = new UserCreateService(usersRepository)
     await userCreateService.execute({ name, email, password })
-
     return response.status(201).json({ message: 'user-created' })
   }
 
   async update(request, response) {
     const { id } = request.params
     const requestPayload = request.body
-
     const usersRepository = new UsersRepository()
     const userUpdateService = new UserUpdateService(usersRepository)
     await userUpdateService.execute({ id, requestPayload })
-
     return response.status(201).json({ message: 'user-updated' })
   }
 
   async delete(request, response) {
     const { id } = request.params
     const { password, confirm_password } = request.body
-
     const usersRepository = new UsersRepository()
     const userDeleteService = new UserDeleteService(usersRepository)
     await userDeleteService.execute({ id, password, confirm_password })
-
     return response.status(201).json({ message: 'user-deleted' })
   }
 }
