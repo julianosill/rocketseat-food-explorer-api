bb4e8e450545b516183a386cdca7d705e1ab7d6c
Juliano Sill <sill.juliano@gmail.com>
2023-10-31 17:09:25 -0300

    feat(users): define users' delete business logic

---
+++ src/services/UserDeleteService.js
@@ -0,0 +1,32 @@
+const bcryptjs = require('bcryptjs')
+const AppError = require('../utils/AppError')
+
+class UserDeleteService {
+  constructor(usersRepository) {
+    this.usersRepository = usersRepository
+  }
+
+  async execute({ id, password, confirm_password }) {
+    const user = await this.usersRepository.findById(id)
+
+    if (!user) {
+      throw new AppError('auth/user-not-found')
+    }
+
+    if (password !== confirm_password) {
+      throw new AppError('auth/password-does-not-match')
+    }
+
+    const correctPassword = await bcryptjs.compare(password, user.password)
+
+    if (!correctPassword) {
+      throw new AppError('auth/password-incorrect')
+    }
+
+    const userDeleted = await this.usersRepository.delete({ user })
+
+    return userDeleted
+  }
+}
+
+module.exports = UserDeleteService
