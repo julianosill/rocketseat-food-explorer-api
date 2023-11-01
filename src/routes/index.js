35aaf2ed691a31b7b8e809f54b7ad1b9843c4e02
Juliano Sill <sill.juliano@gmail.com>
2023-11-01 16:39:04 -0300

    create categories routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express')
 
 const usersRouter = require('./usersRoutes')
 const productsRoutes = require('./productsRoutes')
+const categoriesRoutes = require('./categoriesRoutes')
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/products', productsRoutes)
+routes.use('/categories', categoriesRoutes)
 
 module.exports = routes
