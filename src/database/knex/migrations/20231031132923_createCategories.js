8fab6d5df47244df1c6933e566ad29ee070af654
Juliano Sill <sill.juliano@gmail.com>
2023-11-01 11:36:02 -0300

    chore(knex): update migration name

--- src/database/knex/migrations/20231031132923_createCategories.js
+++ src/database/knex/migrations/20231031132923_createCategories.js
@@ -1,9 +1,9 @@
 exports.up = knex => {
-  return knex.schema.createTable('category', table => {
+  return knex.schema.createTable('categories', table => {
     table.increments('id')
     table.string('name').notNullable()
     table.string('slug').notNullable()
   })
 }
 
-exports.down = knex => knex.schema.dropTable('category')
+exports.down = knex => knex.schema.dropTable('categories')
