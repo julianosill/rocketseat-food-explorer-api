1f64996ff6848a7a57678ede4da54de5a7f79cba
Juliano Sill <sill.juliano@gmail.com>
2023-11-01 16:41:05 -0300

    add update category handler

---
+++ src/services/CategoryUpdateService.js
@@ -0,0 +1,30 @@
+const AppError = require('../utils/AppError')
+
+class CategoryUpdateService {
+  constructor(categoriesRepository) {
+    this.categoriesRepository = categoriesRepository
+  }
+
+  async execute({ id, name }) {
+    const category = await this.categoriesRepository.findById(id)
+
+    if (!name) {
+      throw new AppError('category/name-is-missing')
+    }
+
+    if (!category) {
+      throw new AppError('category/category-not-found')
+    }
+
+    const categoryExists = await this.categoriesRepository.findByName(name)
+    if (categoryExists && categoryExists.id != id) {
+      throw new AppError('category/name-already-exists')
+    }
+
+    await this.categoriesRepository.update({ id, name })
+
+    return category
+  }
+}
+
+module.exports = CategoryUpdateService
