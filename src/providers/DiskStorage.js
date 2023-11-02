ade9e399a4f34f072869640c32793d0caec8b139
Juliano Sill <sill.juliano@gmail.com>
2023-11-02 19:28:31 -0300

    build(upload): setup upload configs

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+const { TMP_FOLDER, UPLOADS_FOLDER } = require('../configs/upload')
+
+class DiskStorage {
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(TMP_FOLDER, file),
+      path.resolve(UPLOADS_FOLDER, file)
+    )
+    return file
+  }
+
+  async deleteFile(file) {
+    const filePath = path.resolve(UPLOADS_FOLDER, file)
+    try {
+      await fs.promises.stat(filePath)
+    } catch (error) {
+      return
+    }
+    await fs.promises.unlink(filePath)
+  }
+}
+
+module.exports = DiskStorage
